c83aea9741b99af7283ebae391a3161efd3bb983
DudeUnoob <to.baladev@gmail.com>
2022-09-08 23:13:05 -0500

    this works fs

--- middleware/mydocuments.js
+++ middleware/mydocuments.js
@@ -2,31 +2,15 @@ const session = require('express-session')
 const path = require('path')
 
 function myDocuments (req, res, next) {
-    // if(!req.session.userid || !req.session.token) {
+    if(!req.session.userid || !req.session.token) {
         
-    //     return res.sendFile('notauthorized.html', { root: path.join(__dirname, '../views/404')})
-    // } else {
-    //     return next()
-    // }
-
-
-
-    if(!req.body.token || !req.session.token || !req.session.userid){
-
-        return res.sendFile('notloggedin.html', { root: path.join(__dirname, '../views/404')})
+        return res.sendFile('notauthorized.html', { root: path.join(__dirname, '../views/404')})
     } else {
-        try{
-            let decrypt = jwt.verify(token, 'helloworld')
-
-        req.user = decrypt;
-        }
-        catch(e) {
-            req.session.destroy()
-            return res.status(400).send('session has expired <a href=/login>Login Again</a>')
-        }
-        
-        return next();
+        return next()
     }
+
+
+    
 }
 
 module.exports = myDocuments;
