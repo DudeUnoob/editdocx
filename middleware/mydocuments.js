7a97379524335a332ff5b00b61c7e1d4d1104b42
DudeUnoob <to.baladev@gmail.com>
2022-09-08 21:57:04 -0500

    updated the document

--- middleware/mydocuments.js
+++ middleware/mydocuments.js
@@ -2,11 +2,31 @@ const session = require('express-session')
 const path = require('path')
 
 function myDocuments (req, res, next) {
-    if(!req.session.userid || !req.session.token) {
+    // if(!req.session.userid || !req.session.token) {
         
-        return res.sendFile('notauthorized.html', { root: path.join(__dirname, '../views/404')})
+    //     return res.sendFile('notauthorized.html', { root: path.join(__dirname, '../views/404')})
+    // } else {
+    //     return next()
+    // }
+
+
+    let token = req.body.token || req.session.token || req.session.userid
+
+    if(!token){
+
+        return res.sendFile('notloggedin.html', { root: path.join(__dirname, '../views/404')})
     } else {
-        return next()
+        try{
+            let decrypt = jwt.verify(token, 'helloworld')
+
+        req.user = decrypt;
+        }
+        catch(e) {
+            req.session.destroy()
+            return res.status(400).send('session has expired <a href=/login>Login Again</a>')
+        }
+        
+        return next();
     }
 }
 
